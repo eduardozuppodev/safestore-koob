c54e7faa6bc5ca85a6c05b38ec38442dbce09207
eduardozuppodev <zuppo.edu@gmail.com>
2021-08-14 09:15:19 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 
+import Header from './components/Header';
+
 const App: React.FC = () => {
   return (
     <>
-      <div>Ola</div>
+      <Header />
       <GlobalStyles />
     </>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
+import { colors } from '../../styles/colors';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 200px;
+
+  background: ${colors.header.background};
+`;
