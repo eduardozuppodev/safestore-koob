a9f91c1240c053a98ef4b31a7549df0bacb0e906
eduardozuppodev <zuppo.edu@gmail.com>
2021-08-15 12:35:56 -0300

    listing products

--- src/components/ListProducts/index.tsx
+++ src/components/ListProducts/index.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import { CardProduct } from '../../components';
 
 import { Container, Content } from './styles';
 
+import api from '../../services/api';
+
+type ProductType = {
+  _id: string;
+  name: string;
+  brand: string;
+  quantity: number;
+  price: string;
+  category: string;
+};
+
 export const ListProducts: React.FC = () => {
+  // Listando Produtos
+
+  //Declarando state
+  const [products, setProducts] = useState([]);
+
+  // Pegando dados do endpoint
+  useEffect(() => {
+    api.get('products').then(({ data }) => {
+      setProducts(data);
+    });
+  }, []);
+
   return (
     <Container>
       <Content>
-        <CardProduct name={'Tênis Esportivo'} brand={'Nike'} quantity={15} price={'20.00'} category={'Calçados'} />
-        <CardProduct name={'Camisa PSG'} brand={'Nike'} quantity={2} price={'200.00'} category={'Camisetas'} />
-        <CardProduct name={'Boné New Era'} brand={'New Era'} quantity={22} price={'150.00'} category={'Bonés'} />
-        <CardProduct name={'Moletom Lacoste'} brand={'Lacoste'} quantity={3} price={'420.00'} category={'Moletons'} />
-        <CardProduct
-          name={'Chinelo Havaianas'}
-          brand={'Havaianas'}
-          quantity={15}
-          price={'24.99'}
-          category={'Chinelos'}
-        />
-        <CardProduct name={'Tênis Esportivo'} brand={'Puma'} quantity={156} price={'200.00'} category={'Calçados'} />
+        {products.map((product: ProductType, id: number) => (
+          <CardProduct
+            key={id}
+            name={product.name}
+            brand={product.brand}
+            quantity={product.quantity}
+            price={product.price}
+            category={product.category}
+          />
+        ))}
       </Content>
     </Container>
   );
 };
+//<CardProduct name={'Tênis Esportivo'} brand={'Nike'} quantity={15} price={'20.00'} category={'Calçados'} />
